22d6cab6c3ff66c5b72a9da8300c8e185f74959a
andressasmr <andressasamaratasca@gmail.com>
2022-01-28 23:27:11 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    JogoDaVelhaModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
